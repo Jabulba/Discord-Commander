d74a998db05452c29a97e1524db176a285742b08
Caio Jabulka <Jabulba@users.noreply.github.com>
2018-02-04 01:39:39 -0200

    Create Randomizer.java

---
+++ src/main/java/com/jabulba/util/Randomizer.java
@@ -0,0 +1,76 @@
+package com.jabulba.util;
+
+import java.util.Random;
+
+public class Randomizer
+{
+	private static Random random = new Random();
+
+	private static long lastCycle = System.currentTimeMillis();
+
+	/**
+	 * @see Random#nextInt()
+	 */
+	public static int nextInt()
+	{
+		cycleRandomness();
+		return random.nextInt();
+	}
+
+	/**
+	 * @see Random#nextInt(int)
+	 */
+	public static int nextInt(int i)
+	{
+		cycleRandomness();
+		return random.nextInt(i);
+	}
+
+	/**
+	 * @see Random#nextFloat()
+	 */
+	public static float nextFloat()
+	{
+		cycleRandomness();
+		return random.nextFloat();
+	}
+
+	/**
+	 * @see Random#nextDouble()
+	 */
+	public static double nextDouble()
+	{
+		cycleRandomness();
+		return random.nextDouble();
+	}
+
+	/**
+	 * @see Random#nextBoolean()
+	 */
+	public static boolean nextBoolean()
+	{
+		cycleRandomness();
+		return random.nextBoolean();
+	}
+
+	private static void cycleRandomness()
+	{
+		if (System.currentTimeMillis() - lastCycle > 1500)
+		{
+			random = new Random();
+			lastCycle = System.currentTimeMillis();
+		}
+	}
+
+	/**
+	 * Randomizes a chance between 0 and 100
+	 *
+	 * @param chance
+	 * 		the % chance to be true [0-100]
+	 */
+	public static boolean chancePercent(double chance)
+	{
+		return Randomizer.nextDouble() < chance / 100;
+
+	}
+}
